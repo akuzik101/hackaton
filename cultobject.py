9e8cfbfce6f3e5fc7a165736df8a8db10817d583
Aleksandrs Kuziks <somson4444@outlook.com>
2022-11-02 14:37:29 +0200

    Added displaying more information about objects

--- cultobject.py
+++ cultobject.py
@@ -1,23 +1,54 @@
 from config import Fields
+from typing import Any
+
+
+class Field:
+    value: Any
+
+    def __init__(self, table_number: int, display_name: str | None = None):
+        self.table_number = table_number
+        self.display_name = display_name
+
+    def set_value(self, data: list):
+        self.value = data[self.table_number]
+
+    def __str__(self):
+        if self.value is None or self.display_name is None:
+            return ''
+        return f'{self.display_name}: {self.value}'
 
 
 class CultObject:
     def __init__(self, query_res: list):
-        self.id = query_res[Fields.ID]
-        self.obj = query_res[Fields.OBJ]
-        self.name = query_res[Fields.NAME]
-        # ISIL
-        self.address = query_res[Fields.ADDRESS]
-        self.lat = query_res[Fields.LAT]
-        self.lon = query_res[Fields.LON]
-        # LKS-92 X
-        # LKS-92 Y
-        self.category = query_res[Fields.CATEGORY]
-        self.phone = query_res[Fields.PHONE]
-        self.email = query_res[Fields.EMAIL]
-        # ATVK
-        self.url = query_res[Fields.URL]
-        self.image_url = query_res[Fields.IMG_URL]
+        self.data = {
+
+            'id': Field(Fields.ID, None),
+            'obj': Field(Fields.OBJ, None),
+            'name': Field(Fields.NAME, 'Nosaukums'),
+            # ISIL
+            'address': Field(Fields.ADDRESS, 'Adrese'),
+            'lat': Field(Fields.LAT, None),
+            'lon': Field(Fields.LON, None),
+            # LKS-92 X
+            # LKS-92 Y
+            'category': Field(Fields.CATEGORY, None),
+            'phone': Field(Fields.PHONE, 'Tālrunis'),
+            'email': Field(Fields.EMAIL, 'E-pasts'),
+            # ATVK
+            'url': Field(Fields.URL, 'Mājaslapa'),
+            'image_url': Field(Fields.IMG_URL, None)
+        }
+        for field in self.data.values():
+            field.set_value(query_res)
 
     def __str__(self):
-        return self.name
+        result = ''
+        for field in self.data.values():
+            if field.display_name:
+                result += str(field) + '\n'
+        return result
+
+
+if __name__ == '__main__':
+    co = CultObject([x for x in range(40)])
+    print(co.data['lat'].value)
